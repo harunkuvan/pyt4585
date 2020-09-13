1210e1f9482da8efbda086f9c5ba40de6a59fa01
harunkuvan <harunkuvan@gmail.com>
2020-09-13 19:34:12 +0300

    Birinci Gun

---
+++ convert.py
@@ -0,0 +1,39 @@
+# Convert : elinizdeki bir veri tipini farkli bir veri tipine cevirme islemi yapar
+
+# int() : tam sayi veri tipine cevirir
+# str() : string (metinsel) degere cevirme islemi yapar
+# float() : ondalikli degere cevirme islemi
+# chr() : verdiginiz sayisal degerin, karakter karsiligini temsil eder.
+# ord() : verdiginiz karakterin, ascii (sayisal) karsiligini temsil eder.
+# ctrl + k + c => yorum satirina alir
+# ctrl + k + u => yorum satirindan alir
+# alt + shift + f => kodlari duzenle
+
+
+# sayi1 = input("Lütfen birinci sayiyi giriniz : ")
+# sayi2 = input("Lütfen ikinci sayiyi giriniz : ")
+
+# # print(sayi1)
+# # print(type(sayi1))
+
+# # print(sayi1 + sayi2)
+
+
+
+# toplam = float(sayi1) + float(sayi2)
+# print(toplam)
+
+# result = int(sayi1) + int(sayi2)
+# print(result)
+
+print(chr(65), ord('A'))
+print(chr(90), ord('Z'))
+print(chr(97), ord('a'))
+print(chr(122), ord('z'))
+
+
+sayi = 100
+
+print("Sayinin degeri\n"  + str(sayi))
+
+
---
+++ variables.py
@@ -0,0 +1,50 @@
+# yorum satiri
+print("Hello Dunya")
+
+
+# degisken tanimlama kurallari
+# 1) degiskenin ismi sayi ile baslayamaz
+# 2) degisken 2 veya fazla kelimeden olusamaz, olusacak ise ornek
+# benim_kullaniciAdim = kahramanmaraslimustafa
+# 3) degisken tanimlamasi yapilirken kesinlikle tanimli kelime kullanilamaz
+# 4) degisken adinda lutfen rica ediyorum turkce karakter kullanmayiniz
+
+benim_adim = "murat vuranok"
+mail_adresim = "murat.vuranok@bilgeadam.com"
+adim = "murat"
+soyadim = 'vuranok'
+
+#int, string, float
+sayi = 5 #int tam sayi veri tipi
+print(sayi)
+print(type(sayi))
+
+print(adim)
+print(soyadim)
+
+print(adim + " " + soyadim) #murat vuranok
+print(adim,soyadim)
+
+fullname = adim + " " + soyadim
+print("kullanicinin adi soyadi:",fullname)
+
+x = True
+y = False
+print(x)
+print(y)
+print(type(x))
+
+print((fullname + "\n")*5)
+
+print("""
+bilge
+adam
+yazilim
+kursu
+""")
+
+print("bilge adam \"besiktas\" yazilim dersleri istanbul\n\
+    python kursu\
+    test satiri")
+
+
